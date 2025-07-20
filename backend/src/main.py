226f1704043e626865fb7bc78292c9652bf97900
zorig <zorigto.rd37122@gmail.com>
2025-07-20 10:49:59 +0800

    frotend setup and installing ant design, axios 2

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,8 +1,14 @@
 import uvicorn
 from fastapi import FastAPI
 from src.routers import main_router
+import sys
 app = FastAPI()
 app.include_router(main_router)
 
+@app.get("/")
+async def root():
+    return {"message": "API works!"}
+
 if __name__ == '__main__':
-    uvicorn.run("src.main:app", host="0.0.0.0",  reload=True)
+    print(sys.path)
+    uvicorn.run("src.main:app",  reload=True)
